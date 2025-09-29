0314aecc9c089b61fd8de4c11d7ab25061896ea1
Priya Mishra <119393765+developer-priya-mishra@users.noreply.github.com>
2025-09-29 14:53:35 +0530

    "Some changes"

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,19 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resumind" },
+    { name: "description", content: "Smart feedbacl for your dream!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main>
+    <section className="main-section">
+      <div className="page-heading">
+        <h1>Track Your Applications & Resume Ratings</h1>
+        <h2>Review your submissions and check AI-powered feedback.</h2>
+      </div>
+    </section>
+  </main>
 }
